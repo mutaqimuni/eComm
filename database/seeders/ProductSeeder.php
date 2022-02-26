2b1666416bcc0cd52a967e962f4e5cbca24cb5bb
mutaqimuni <mutaqim.unijaya@gmail.com>
2022-02-26 12:17:27 +0800

    Fix tak dapat masukkan data banyak2 waktu seeder.Array to kene ada dua

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -15,11 +15,34 @@ class ProductSeeder extends Seeder
     public function run()
     {
         DB::table('products')->insert([
+        [
             'name'=> 'LG mobile',
+            'price' =>'100',
+            'description' => 'Can make you very happy with technology',
+            'category'=> 'mobile',
+            'gallery'=> 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.lg.com%2Flevant_en%2Fsmartphones&psig=AOvVaw0U6Y-pDvqqpNPPWheJeOhG&ust=1645934786384000&source=images&cd=vfe&ved=0CAgQjRxqFwoTCOi8goq_nPYCFQAAAAAdAAAAABAJ'
+        ],
+        [
+            'name'=> 'Sony mobile',
             'price' =>'200',
             'description' => 'Can make you very happy with technology',
             'category'=> 'mobile',
             'gallery'=> 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.lg.com%2Flevant_en%2Fsmartphones&psig=AOvVaw0U6Y-pDvqqpNPPWheJeOhG&ust=1645934786384000&source=images&cd=vfe&ved=0CAgQjRxqFwoTCOi8goq_nPYCFQAAAAAdAAAAABAJ'
+        ],
+        [
+            'name'=> 'Oppo mobile',
+            'price' =>'300',
+            'description' => 'Can make you very happy with technology',
+            'category'=> 'mobile',
+            'gallery'=> 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.lg.com%2Flevant_en%2Fsmartphones&psig=AOvVaw0U6Y-pDvqqpNPPWheJeOhG&ust=1645934786384000&source=images&cd=vfe&ved=0CAgQjRxqFwoTCOi8goq_nPYCFQAAAAAdAAAAABAJ'
+        ],
+        [
+            'name'=> 'Samsung mobile',
+            'price' =>'400',
+            'description' => 'Can make you very happy with technology',
+            'category'=> 'mobile',
+            'gallery'=> 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.lg.com%2Flevant_en%2Fsmartphones&psig=AOvVaw0U6Y-pDvqqpNPPWheJeOhG&ust=1645934786384000&source=images&cd=vfe&ved=0CAgQjRxqFwoTCOi8goq_nPYCFQAAAAAdAAAAABAJ'
+        ]
         ]);
     }
 }
