dc45bb1a4f93fc79e45f17f92cfe14eb8ec4e79c
mutaqimuni <mutaqim.unijaya@gmail.com>
2022-02-26 23:59:18 +0800

    Import Product dan test pass ke landing page

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,11 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
     function index(){
-        return "Welcome to our index page";
+        return Product::all();
     }
 }
